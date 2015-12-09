11539024752719a0f4fb1b9397151cae8da10c66
joshwyatt <joshuawyatt123@gmail.com>
2015-12-08 16:50:16 -0800

    Add initial component

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,10 @@
 // Your ReactJS code here
+var App = React.createClass({
+  render: function () {
+    return <div>Hello</div>
+  }
+});
+
+
+React.render(<App />, document.getElementById('app'));
 
--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
+console.log('listening on 3000');
