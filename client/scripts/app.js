6923c28e8afe51fcb642e2d7eb1d6dd8eb21117d
joshwyatt <joshuawyatt123@gmail.com>
2015-12-08 18:15:55 -0800

    Add bare bones player
    Unfortunately this player does not seem to work on Chrome.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,8 @@
-// Your ReactJS code here
+SC.initialize({
+  client_id: SOUND_CLOUD_KEY,
+  redirect_uri: 'localhost:3000/'
+});
+
 var App = React.createClass({
   render: function () {
     return <Song name='yamoms'></Song>
@@ -9,6 +13,12 @@ var Song = React.createClass({
   render: function () {
     return <div className="song">Title: { this.props.name }</div>
   }
-})
+});
+
+SC.stream('/tracks/49438146').then(function (player) {
+  yaMomsPlayer = player;
+  player.play();
+});
+
 
 React.render(<App />, document.getElementById('app'));
