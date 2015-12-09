3bc2f7f59f258f5355e6a6d34bf6cdf272ff22d6
joshwyatt <joshuawyatt123@gmail.com>
2015-12-08 17:03:38 -0800

    Add bare bones song component

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,14 @@
 // Your ReactJS code here
 var App = React.createClass({
   render: function () {
-    return <div>Hello</div>
+    return <Song name='yamoms'></Song>
   }
 });
 
+var Song = React.createClass({
+  render: function () {
+    return <div className="song">Title: { this.props.name }</div>
+  }
+})
 
 React.render(<App />, document.getElementById('app'));
-
